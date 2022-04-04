ea836c6be071cee22e1678eca84ad4b4cd1c3eeb
Theodor <dev.thevrbata@gmail.com>
2022-04-04 16:13:19 +0300

    Update bloodboil.js

--- bloodboil.js
+++ bloodboil.js
@@ -71,6 +71,7 @@ class Bloodboil {
     {
         const response = await fetch("https://open-api.trovo.live/openplatform/channels/id", requestOptions);
         const streamerInfo = await response.json();
+        console.log(streamerInfo);
 
         return streamerInfo.hasOwnProperty('is_live') && streamerInfo.is_live === true;
     }
