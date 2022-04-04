a9020e86425c157fff9330603d0522597b2cb54a
Theodor <dev.thevrbata@gmail.com>
2022-04-04 16:31:24 +0300

    Update bloodboil.js

--- bloodboil.js
+++ bloodboil.js
@@ -17,7 +17,7 @@ class Bloodboil {
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             } else {
                 for (const streamer of this.streamerOrder) {
-                    console.log(this.checkOnline(streamer).then(online => { return online; });
+                    console.log(this.checkOnline(streamer).then(online => { return online; }));
 //                     if (if (this.checkOnline(streamer).then(online => { return online; }) === true) {) {
 //                         streamerName = streamer;
 //                         break;
